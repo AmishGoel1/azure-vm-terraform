76e95b06826a75a018f93481ff049f99bc4c6d78
Amish Goel <amishgoel1@icloud.com>
2025-05-14 22:03:03 -0700

    added vnet peering resource blocks

--- main.tf
+++ main.tf
@@ -242,3 +242,16 @@ resource "azurerm_linux_virtual_machine" "Vm2" {
     storage_account_uri = azurerm_storage_account.storage_account_vm2.primary_blob_endpoint
   }
 }
+resource "azurerm_virtual_network_peering" "peer-1" {
+  name                      = "peer1to2"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.my_terraform_network.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet2_vm2.id
+}
+
+resource "azurerm_virtual_network_peering" "peer-2" {
+  name                      = "peer2to1"
+  resource_group_name       = azurerm_resource_group.rg_vm2.name
+  virtual_network_name      = azurerm_virtual_network.vnet2_vm2.name
+  remote_virtual_network_id = azurerm_virtual_network.my_terraform_network.id
+}
